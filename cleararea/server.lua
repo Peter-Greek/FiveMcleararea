76dcf327b5a4390be8066f5a67a1c363f2a711e6
Peter Greek <50223929+Xerxes468893@users.noreply.github.com>
2019-05-23 16:13:43 -0400

    Update server.lua

--- cleararea/server.lua
+++ cleararea/server.lua
@@ -2,7 +2,7 @@ RegisterCommand(clearall, function(source, args, rawCommand)
 	local player = source	 
 	print("Cleared area")
 		TriggerClientEvent("CA", -1)
-		TriggerClientEvent('chatMessage', -1, '[^1SYSTEM^0]', { 255, 255, 255 }, GetPlayerName(player) ..' Cleared all vehicles from the Server')
+		TriggerClientEvent('chat:addMessage', -1, '[^1SYSTEM^0]', { 255, 255, 255 }, GetPlayerName(player) ..' Cleared all vehicles from the Server')
 	end, true) --Set to False if you want to let everyone use it, for aces add_ace group.admin command.clearall allow in a config to give perms
 
 
